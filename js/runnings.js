55fb2052f43e2bf34b14d55a071b8b4107e211a4
kongou-ae <y.matsumoto.ae@gmail.com>
2015-04-04 23:13:41 +0900

    ipdate

--- js/runnings.js
+++ js/runnings.js
@@ -6,7 +6,7 @@ exports.header = {
         return Null;
         break;
       default:
-        return "<div style='margin:0px 25px 0px 25px;font-size:0.75em;color:#808080;font-family:IPAexGothic;line-height: 1.5;'>ほげほげサービス　サービス仕様書<hr></div>"
+        return "<div style='margin:0px 45px 0px 45px;font-size:0.75em;color:#808080;font-family:IPAexGothic;line-height: 1.5;'><br>ほげほげサービス　サービス仕様書<hr></div>"
         break;
     }
   }
@@ -15,7 +15,7 @@ exports.header = {
 exports.footer = {
   height   : "2cm",
   contents : function(pageNum, numPages) {
-    defFooter = "<hr style='margin:0px 25px 0px 25px'><div style='text-align:center;font-size:0.75em;color:#808080;font-family:IPAexGothic;line-height:1.5;'>Copyright 2015 hogehoge CO., LTD. All rights reserved.<br>"
+    defFooter = "<hr style='margin:0px 45px 0px 45px'><div style='text-align:center;font-size:0.75em;color:#808080;font-family:IPAexGothic;line-height:1.5;'>Copyright 2015 hogehoge CO., LTD. All rights reserved.<br>"
     switch(pageNum){
       case 1:
         return Null;
