68c828e6b5e0dbdaa9e836da016ad59b9aebbd85
kongou-ae <y.matsumoto.ae@gmail.com>
2015-04-06 23:01:52 +0900

    change element of CSS to variables

--- js/runnings.js
+++ js/runnings.js
@@ -1,12 +1,19 @@
 exports.header = {
   height   : "2cm",
   contents : function(pageNum, numPages){
+    // variable
+    docTitle = "ほげほげサービス　サービス仕様"
+    margin = "0px 45px 0px 45px;"
+    fontSize = "0.75em;"
+    fontColor = "#808080;"
+    fontFamily = "IPAexGothic;"
+    // main
     switch(pageNum){
       case 1:
         return Null;
         break;
       default:
-        return "<div style='margin:0px 45px 0px 45px;font-size:0.75em;color:#808080;font-family:IPAexGothic;line-height: 1.5;'><br>ほげほげサービス　サービス仕様書<hr></div>"
+        return "<div style='margin:" + margin + "font-size:" + fontSize + "color:" + fontColor + "font-family:" + fontFamily + "line-height:1.5;'><br>" + docTitle + "<hr></div>"
         break;
     }
   }
@@ -15,21 +22,28 @@ exports.header = {
 exports.footer = {
   height   : "2cm",
   contents : function(pageNum, numPages) {
-    defFooter = "<hr style='margin:0px 45px 0px 45px'><div style='text-align:center;font-size:0.75em;color:#808080;font-family:IPAexGothic;line-height:1.5;'>Copyright 2015 hogehoge CO., LTD. All rights reserved.<br>"
+    // variable
+    copyRight = "Copyright 2016 hogehoge CO., LTD. All rights reserved."
+    margin = "0px 45px 0px 45px;"
+    fontSize = "0.75em;"
+    fontColor = "#808080;"
+    fontFamily = "IPAexGothic;"
+    // main
+    defaultFooter = "<hr style='margin:" + margin + "'><div style='text-align:center;font-size:" + fontSize + "color:" + fontColor + "font-family:" + fontFamily + "line-height:1.5;'>" + copyRight + "<br>"
     switch(pageNum){
       case 1:
         return Null;
         break;
       case 2:
-        return defFooter + "ⅰ</div>"
+        return defaultFooter + "ⅰ</div>"
         break;
       case 3:
-        return defFooter + "ⅱ" + "</div>"
+        return defaultFooter + "ⅱ" + "</div>"
         break;
       default:
         pageNum = pageNum - 3
         numPages = numPages - 3
-        return defFooter + pageNum + " / " + numPages + "</div>"
+        return defaultFooter + pageNum + " / " + numPages + "</div>"
         break;
     }
   }
